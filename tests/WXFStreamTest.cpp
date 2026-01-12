21209e3e985fe09d4a033298d67dc498395737d0
Kevin Chen <chenty0704@gmail.com>
2026-01-12 16:02:22 -0500

    Changed integer width to 64 bits when reading WXF stream.

--- tests/WXFStreamTest.cpp
+++ tests/WXFStreamTest.cpp
@@ -12,7 +12,7 @@ TEST(InWXFStreamTest, BasicInput) {
     LLU::InWXFStream stream("resources/TestData.wxf");
     EXPECT_EQ(stream.Length(), 4);
 
-    int intVal;
+    int64_t intVal;
     stream >> intVal;
     EXPECT_EQ(intVal, 1024);
 
@@ -20,13 +20,13 @@ TEST(InWXFStreamTest, BasicInput) {
     stream >> realVal;
     EXPECT_EQ(realVal, 3.14);
 
-    vector<int> list;
+    vector<int64_t> list;
     stream >> list;
-    EXPECT_EQ(list, views::iota(0, 10) | ranges::to<vector>());
+    EXPECT_EQ(list, views::iota(0, 10) | ranges::to<vector<int64_t>>());
 
-    mdarray<int, dims<2>> matrix;
+    mdarray<int64_t, dims<2>> matrix;
     stream >> matrix;
-    EXPECT_EQ(matrix.container(), vector({
+    EXPECT_EQ(matrix.container(), vector<int64_t>({
                   1, 0, 0,
                   0, 1, 0,
                   0, 0, 1}));
